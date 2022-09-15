147c1650972830a8f4b5694a7f3434c36ad412ac
Tomdom <noritorihotondo@wp.pl>
2022-09-15 14:04:38 +0200

    Path imports facilitation and changing let to const in route interceptor

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,10 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express from 'express';
 import preMiddlewares from './middlewares/pre/index';
-import { BaseError } from './lib/utils/base-error';
 import { ErrorHandler } from './lib/utils/error-handler';
 import { logger } from './lib/utils/logger';
-import { APIError } from './lib/utils/api-error';
 import routes from './routes';
 import { respondSuccess, validateRequestPayload } from './lib/utils/http';
+import { errorMiddleware } from './middlewares/errorMiddleware';
 
 const app = express();
 
@@ -13,7 +12,7 @@ for (const middleware of preMiddlewares) {
   app.use(...middleware);
 }
 
-for (let route of routes) {
+for (const route of routes) {
   app[route.method](route.url, async (req, res, next) => {
     try {
       if (route.schema) {
@@ -39,22 +38,6 @@ process.on('uncaughtException', async (error: Error) => {
 process.on('unhandledRejection', (reason: Error) => {
   throw reason;
 });
-
-export const errorMiddleware = async (
-  err: APIError | BaseError,
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  if (!errorHandler.isTrustedAndBaseError(err)) {
-    next(err);
-  }
-  const message = err instanceof APIError ? err.message : 'Initial server Error';
-  res
-    .status((<BaseError>err)?.httpCode || 500)
-    .send({ error_code: err.errorCode, message: err.message, http_code: err.httpCode });
-  await errorHandler.handleError(err);
-};
 
 app.use(errorMiddleware);
 
