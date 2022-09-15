94c8e50b493f2cdf4678b2cb4192898af5121be1
Tomdom <noritorihotondo@wp.pl>
2022-09-15 21:30:36 +0200

    Delete closeWithError function, add APIError, facilitating imports' paths

--- src/lib/utils/http.ts
+++ src/lib/utils/http.ts
@@ -1,52 +1,53 @@
-import {AnySchema} from 'joi';
-import {Response} from 'express';
-
-import {HTTPCode} from '../../types/HTTP/http.status';
-
+import { Response } from 'express';
+import { AnySchema } from 'joi';
+import { HTTPCode, ApiErrorCode } from '../../types';
 import Config from '../../lib/utils/config';
-import {APIError} from '../../lib/utils/api-error';
-import {ApiErrorCode} from "../../types/HTTP/http.model";
+import { APIError } from './api-error';
 
-export function respondSuccess (res: Response, data: unknown, code = HTTPCode.OK) {
-    res.setHeader('Content-Type', 'application/json');
+export function respondSuccess(res: Response, data: unknown, code = HTTPCode.OK) {
+  res.setHeader('Content-Type', 'application/json');
 
-    const response = { status: 'ok', data };
-    const parsed = Config.NODE_ENV === 'development' ?
-        JSON.stringify(response, null, 4) :
-        JSON.stringify(response);
+  const response = { status: 'ok', data };
+  const parsed =
+    Config.NODE_ENV === 'development'
+      ? JSON.stringify(response, null, 4)
+      : JSON.stringify(response);
 
-    return res.status(code).send(parsed);
+  return res.status(code).send(parsed);
 }
 
-export function closeWithError (res: Response, error: APIError) {
-    const response = { status: 'error', message: error.message };
+export function validateRequestPayload(body: any, schema: AnySchema): Promise<any> {
+  const buildPath = (path: (string | number)[]) => {
+    return (
+      path.reduce((p, n) => {
+        p += typeof n === 'string' ? `.${n}` : `[${n}]`;
 
-    res.setHeader('Content-Type', 'application/json');
+        return p;
+      }, '') as string
+    ).slice(1);
+  };
 
-    return res.status(error.errorCode).send(response);
-}
-
-export function validateRequestPayload (body: any, schema: AnySchema): Promise<any> {
-    const buildPath = (path: (string | number)[]) => {
-        return (path.reduce((p, n) => {
-            p += typeof n === 'string' ? `.${n}` : `[${n}]`;
-
-            return p;
-        }, '') as string).slice(1);
-    };
-
-    return new Promise((resolve, reject) => {
-        const { error, value } = schema.validate(body, {
-            convert: false,
-            stripUnknown: true
-        });
-        if (error) {
-            const message = `Request validation failed: ${error.details[0].message} (${buildPath(error.details[0].path)})`;
-
-            return reject(new APIError(message, HTTPCode.BAD_REQUEST, true, ApiErrorCode.BadCredentials, 'validation'));
-        }
-
-        return resolve(value);
+  return new Promise((resolve, reject) => {
+    const { error, value } = schema.validate(body, {
+      convert: false,
+      stripUnknown: true,
     });
-}
+    if (error) {
+      const message = `Request validation failed: ${error.details[0].message} (${buildPath(
+        error.details[0].path,
+      )})`;
 
+      return reject(
+        new APIError(
+          message,
+          HTTPCode.BAD_REQUEST,
+          true,
+          ApiErrorCode.BadCredentials,
+          'validation',
+        ),
+      );
+    }
+
+    return resolve(value);
+  });
+}
